9b7bf0c0444de60113b82c9fbe5f9509fbda7c9b
asapovk <asapovk@gmail.com>
2018-03-24 01:45:16 +0300

    made creating comment replies

--- node_js/comment-box/src/components/Comment/index.js
+++ node_js/comment-box/src/components/Comment/index.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {deleteComment, toggleInput, selectStatus, commentSelector, updateComment, loadComments} from '../../ducks/comments'
+import {deleteComment, toggleInput, selectStatus, commentSelector, updateComment, loadComments, createComment} from '../../ducks/comments'
 import InputBox from '../InputBox'
 import CommentList from '../CommentList'
 
 class Comment extends Component {
 
   state = {
-    showReply: false
+    showReply: false,
+    showReplyInputBox: false
   }
 
   handleDeleteComment = () => {
@@ -31,8 +32,18 @@ class Comment extends Component {
     }
     else return <div>{text}</div>
   }
+  toggleReplyBox = () => {
+    this.setState({showReplyInputBox: !this.state.showReplyInputBox})
+    this.props.toggleInput(null)
+  }
+  renderReplyInputBox = () => {
+    if(this.state.showReplyInputBox) return <InputBox startValue={null} onSubmitComment={this.handleReplyComment}/>
+  }
   renderReplyComments = (comments) => {
     if(comments && this.state.showReply) return <CommentList comments={this.props.comments}/>
+  }
+  handleReplyComment = (text) => {
+    this.props.createComment({user: 'asapovk', text, article: this.props.id})
   }
   render() {
     const {user, text, comments} = this.props
@@ -43,9 +54,11 @@ class Comment extends Component {
         <button onClick={this.handleDeleteComment}>Delete</button>
         <button onClick={this.toggleInputBox}>Edit</button>
         <button onClick={this.toggleReply}>Show reply</button>
+        <button onClick={this.toggleReplyBox}>Reply</button>
+        {this.renderReplyInputBox()}
         {this.renderReplyComments(comments)}
       </div>
     )
   }
 }
-export default connect((state, ownProps)=>({status: selectStatus(ownProps.id, state.commentReducer.input), comments: commentSelector(state, ownProps.id),  ...ownProps}), {deleteComment, toggleInput, updateComment, loadComments})(Comment)
+export default connect((state, ownProps)=>({status: selectStatus(ownProps.id, state.commentReducer.input), comments: commentSelector(state, ownProps.id),  ...ownProps}), {deleteComment, toggleInput, updateComment, loadComments, createComment})(Comment)
