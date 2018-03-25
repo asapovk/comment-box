d14e4008d544645240c1600124fb3031a9993de0
asapovk <asapovk@gmail.com>
2018-03-25 23:17:52 +0300

    made simple auth

--- node_js/comment-box/src/ducks/auth.js
+++ node_js/comment-box/src/ducks/auth.js
@@ -1,14 +1,55 @@
+import axios from 'axios'
+import {Record} from 'immutable'
+
+const SIGN_IN = 'auth/SIGN_IN'
+const LOGIN = 'auth/LOGIN'
+const SIGN_UP = 'auth/SIGN_UP'
+const REGISTER = 'auth/REGISTER'
 
 
-export const signIn = () => {
+export const signIn = (email, password) => {
+ return (dispatch) => {
+      const response = dispatch({
+      type: SIGN_IN,
+      payload: axios.post('/api/signin', {email, password})})
+      response.then((user)=>{
+        dispatch({type: LOGIN,
+            payload: user
+        })
+      })
 
+  }
 }
 
-export const signUp = () => {
+export const signUp = (email, password) => {
+ return (dispatch) => {
+      const response = dispatch({
+      type: SIGN_UP,
+      payload: axios.post('/api/signup', {email, password})})
+      response.then((user)=>{
+        dispatch({type: REGISTER,
+            payload: user
+        })
+      })
 
+  }
 }
 
+const reducerRecord = Record({
+  user: null
+})
 
-export default function reducer (state={}, action) {
-  return state
+
+
+export default function reducer (state = new reducerRecord(), action) {
+  const {type, payload} = action
+  switch (type) {
+    case REGISTER:
+      return state.set('user', payload)
+    case LOGIN:
+      return state.set('user', payload)
+    default:
+      return state
+  }
+
 }
